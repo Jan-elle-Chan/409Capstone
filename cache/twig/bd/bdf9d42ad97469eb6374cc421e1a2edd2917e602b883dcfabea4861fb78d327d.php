3ba12f8062f4b876345bef313dcf6e4ce51faee8
Jan-elle-Chan <j_michelene@yahoo.com>
2024-03-11 00:37:22 -0700

    Change footer background

--- cache/twig/bd/bdf9d42ad97469eb6374cc421e1a2edd2917e602b883dcfabea4861fb78d327d.php
+++ cache/twig/bd/bdf9d42ad97469eb6374cc421e1a2edd2917e602b883dcfabea4861fb78d327d.php
@@ -32,7 +32,9 @@ class __TwigTemplate_3ff3881a7e87ac6494db2b1ee9aee1f82bc6a1201717d7a3dea6303a161
         // line 2
         echo twig_escape_filter($this->env, ($context["grid_size"] ?? null), "html", null, true);
         echo "\">
-        <p><a href=\"http://getgrav.org\">Grav</a> was <i class=\"fa fa-code\"></i> with <i class=\"fa fa-heart-o pulse \"></i> by <a href=\"https://trilby.media\">Trilby Media</a>.</p>
+        <p><a href=\"https://docs.google.com/forms/d/e/1FAIpQLSdRxBe8XcXRCo2Bli_LJovnxYWVLBbQ8piaRWb9MTh8cMdFXw/viewform?usp=pp_url\">Feedback</p>
+        
+       
     </section>
 </section>
 ";
@@ -65,9 +67,11 @@ class __TwigTemplate_3ff3881a7e87ac6494db2b1ee9aee1f82bc6a1201717d7a3dea6303a161
     {
         return new Source("<section id=\"footer\" class=\"section bg-gray\">
     <section class=\"container {{ grid_size }}\">
-        <p><a href=\"http://getgrav.org\">Grav</a> was <i class=\"fa fa-code\"></i> with <i class=\"fa fa-heart-o pulse \"></i> by <a href=\"https://trilby.media\">Trilby Media</a>.</p>
+        <p><a href=\"https://docs.google.com/forms/d/e/1FAIpQLSdRxBe8XcXRCo2Bli_LJovnxYWVLBbQ8piaRWb9MTh8cMdFXw/viewform?usp=pp_url\">Feedback</p>
+        
+       
     </section>
 </section>
-", "partials/footer.html.twig", "C:\\xampp\\htdocs\\grav-admin\\user\\themes\\quark\\templates\\partials\\footer.html.twig");
+", "partials/footer.html.twig", "C:\\xampp\\htdocs\\dtSimplified\\user\\themes\\quark\\templates\\partials\\footer.html.twig");
     }
 }
